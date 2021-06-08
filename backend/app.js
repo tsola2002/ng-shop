e019f1258d6c9b408301adbf764fc63579b164af
Segun Abolaji <talkense@gmail.com>
2021-06-08 16:45:04 +0100

    created frontend using nx

--- backend/app.js
+++ backend/app.js
@@ -12,20 +12,53 @@ const api = process.env.API_URL;
 app.use(express.json());
 app.use(morgan("tiny"));
 
+// we create a table/schema
+const productSchema = mongoose.Schema({
+  name: String,
+  image: String,
+  countInStock: Number
+});
+
+const Product = mongoose.model('Product', productSchema);
+
 // we initialize the route
 // http://localhost:3000/api/v1/products
-app.get(`${api}/products`, (req, res) => {
-  const product = {
-    id: 1,
-    name: "hair dresser",
-    image: "some_url",
-  };
+app.get(`${api}/products`, async (req, res) => {
+  const productList = await Product.find();
+
+  if (!productList) {
+      res.status(500).json({success: false})
+  }
+  res.send(productList);
+  // const product = {
+  //   id: 1,
+  //   name: "hair dresser",
+  //   image: "some_url",
+  // };
   res.send(product);
 });
 
+// we refactor post 
 app.post(`${api}/products`, (req, res) => {
-  const newProduct = req.body;
-  console.log(newProduct);
+  const product = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    countInStock: {
+      type: Number,
+      required: true
+    }
+  });
+
+  product.save().then((createdProduct => {
+    // return 201 response with created product
+    res.status(201).json(createdProduct)
+  })).catch((err) => {
+    res.status(500).json({
+      error: err,
+      success: false
+    });
+    
+   });
   res.send(newProduct);
 });
 
