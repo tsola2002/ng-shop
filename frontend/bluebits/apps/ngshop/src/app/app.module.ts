30b1e559d474b4acf808fccf51655de87d2b4d61
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-02-25 12:53:08 +0100

    frontend & backend scaffolded

--- frontend/bluebits/apps/ngshop/src/app/app.module.ts
+++ frontend/bluebits/apps/ngshop/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProductListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
